72d69d28bb4793c075ab5ef0c85b6c19f60adc90
Daniel Husar <dano.husar@gmail.com>
2014-06-24 15:57:46 +0100

    app

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+(function(window, $){
+  'use strict';
+
+  $('main').slidescroll({
+    pagesSelector: '.page'
+  });
+
+  $('body').on('click.app', '[data-popup]', function(e){
+    var left = ($(window).width()/2) - (700/2),
+      top = ($(window).height()/2) - (500/2);
+    window.open($(this).attr('href'), $(this).data('share'), 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=700, height=500, top=' + top + ', left=' + left);
+    e.preventDefault();
+  });
+
+})(this, this.jQuery);
